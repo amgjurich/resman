69b3102836346a5a548e319ef002ed316321dc0c
Abigail Gjurich <amgjurich@gmail.com>
2021-08-29 18:31:53 -0400

    trying to fix my fetch requests so I can deploy

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const cors = require('cors');
 const api = require('./server/routes/api.js');
 const { urlencoded } = require('express');
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,13 +12,16 @@ app.use(urlencoded({ extended: true }));
 app.use('/api', api);
 
 //this is what's changing everything on my local computer
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/dist/'));
 
-app.use(express.static(__dirname + '/dist/'));
+  console.log('dirname', __dirname);
 
-console.log('dirname', __dirname);
+  app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+  });
+}
 
-app.get(/.*/, function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
